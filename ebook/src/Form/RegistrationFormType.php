1defb5e11f9020573406860fecbc8a9e4a9bb6fc
MiKL <mikl.gaillard@gmail.com>
2022-07-27 17:15:49 +0200

    eB00Ks

--- ebook/src/Form/RegistrationFormType.php
+++ ebook/src/Form/RegistrationFormType.php
@@ -2,18 +2,19 @@
 
 namespace App\Form;
 
-use ApiPlatform\Core\Action\PlaceholderAction;
 use App\Entity\User;
 use Doctrine\DBAL\Types\DecimalType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use ApiPlatform\Core\Action\PlaceholderAction;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -25,9 +26,9 @@ class RegistrationFormType extends AbstractType
             ->add('firstname',TextType::class,['label'=>'Prénom','attr' => ['class' => 'bgform'],])
             ->add('email',TextType::class,['label'=>'Adresse de courriel','attr' => ['class' => 'bgform'],])
             // ->add('phone')
-            ->add('birthdate')
+            ->add('birthdate',BirthdayType::class,['label'=>'Date de naissance','attr' => ['class' => 'bgform'],])
             ->add('adress',TextType::class,['label'=>'Adresse','attr' => ['class' => 'bgform'],])
-            ->add('zipcode') # ,DecimalType::class,['label'=>'Code postal','attr' => ['class' => 'bgform'],]
+            ->add('zipcode',TextType::class,['label'=>'Code postal','constraints' => new Length(['max' => 5]),'attr' => ['class' => 'bgform'],]) 
             ->add('city',TextType::class,['label'=>'Ville','attr' => ['class' => 'bgform'],])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
